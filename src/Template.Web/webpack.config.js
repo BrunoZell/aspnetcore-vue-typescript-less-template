d5f13404d68efc0ee4d43fb4043b8e8052e91dfd
Bruno Zell <bruno.zzell@gmail.com>
2018-07-31 01:08:49 +0200

    build only one bundle with Webpack

--- src/Template.Web/webpack.config.js
+++ src/Template.Web/webpack.config.js
@@ -1,67 +1,47 @@
 const { VueLoaderPlugin } = require("vue-loader");
 const { TsConfigPathsPlugin } = require("awesome-typescript-loader");
 const path = require("path");
-const webpack = require("webpack");
 
-module.exports = [
-    {
-        name: "vendor",
-        mode: "development",
-        entry: [
+module.exports = {
+    mode: "development",
+    entry: {
+        main: [
+            "babel-polyfill",
             "vue",
             "vue-router",
-            "vuex"
+            "./ClientApp/boot.ts"
         ],
-        output: {
-            path: path.resolve(__dirname, "./wwwroot/dist"),
-            filename: "vendor.js",
-            library: "vendor_[hash]"
-        },
-        plugins: [
-            new webpack.DllPlugin({
-                name: "vendor_[hash]",
-                path: path.resolve(__dirname, "./wwwroot/dist/manifest.json")
-            })
+    },
+    output: {
+        path: path.join(__dirname, "./wwwroot/dist"),
+        filename: "[name].js",
+        publicPath: "dist/"
+    },
+    resolve: {
+        // Makes sure the paths set in tsconfig will load (baseurl)
+        extensions: [".js", ".ts"],
+        plugins: [new TsConfigPathsPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue\.html$/,
+                include: /ClientApp/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.ts$/,
+                include: /ClientApp/,
+                use: ["babel-loader", "awesome-typescript-loader?silent=true"]
+            },
+            {
+                test: /\.less$/,
+                include: /ClientApp/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            }
         ]
     },
-    {
-        name: "main",
-        mode: "development",
-        dependencies: ["vendor"],
-        entry: ["babel-polyfill", "./ClientApp/boot.ts"],
-        output: {
-            path: path.join(__dirname, "./wwwroot/dist"),
-            filename: "[name].js",
-            publicPath: "dist/"
-        },
-        resolve: {
-            // Makes sure the paths set in tsconfig will load (baseurl)
-            extensions: [".js", ".ts"],
-            plugins: [new TsConfigPathsPlugin()]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue\.html$/,
-                    include: /ClientApp/,
-                    loader: "vue-loader"
-                },
-                {
-                    test: /\.ts$/,
-                    include: /ClientApp/,
-                    use: ["babel-loader", "awesome-typescript-loader?silent=true"]
-                },
-                {
-                    test: /\.less$/,
-                    include: /ClientApp/,
-                    use: ["style-loader", "css-loader", "less-loader"]
-                }
-            ]
-        },
-        plugins: [
-            new VueLoaderPlugin(),
-            new webpack.DllReferencePlugin({
-                manifest: path.resolve(__dirname, "./wwwroot/dist/manifest.json")
-            })
-        ]
-    }];
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};

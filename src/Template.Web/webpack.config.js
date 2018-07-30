affdfc78d93a18035fdc23ed0de011d24796b87a
Bruno Zell <bruno.zzell@gmail.com>
2018-07-30 22:56:51 +0200

    updated webpack config - babel removed

--- src/Template.Web/webpack.config.js
+++ src/Template.Web/webpack.config.js
@@ -1,66 +1,41 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const { TsConfigPathsPlugin } = require("awesome-typescript-loader");
 const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const { CheckerPlugin, TsConfigPathsPlugin } = require("awesome-typescript-loader");
-const bundleOutputDir = "./wwwroot/dist";
 
-module.exports = (env) => {
-    const isDevBuild = !(env && env.prod);
-
-    return [{
-        stats: { modules: false },
-        context: __dirname,
-        resolve: {
-            extensions: [".js", ".ts"],
-            plugins: [
-                new TsConfigPathsPlugin()
-            ]
-        },
-        entry: {
-            "main": ["babel-polyfill", "./ClientApp/boot.ts"]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.vue\.html$/, include: /ClientApp/, loader: "vue-loader",
-                    options: {
-                        loaders: {
-                            js: ["babel-loader", "awesome-typescript-loader?silent=true"],
-                            css: ["style-loader", "css-loader", "less-loader"]
-                        }
-                    }
-                },
-                { test: /\.ts$/, include: /ClientApp/, use: ["babel-loader", "awesome-typescript-loader?silent=true"] },
-                { test: /\.less$/, include: /ClientApp/, use: "less-loader" },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: "url-loader?limit=25000" }
-            ]
-        },
-        output: {
-            path: path.join(__dirname, bundleOutputDir),
-            filename: "[name].js",
-            publicPath: "dist/"
-        },
-        plugins: [
-            new CheckerPlugin(),
-            new webpack.DefinePlugin({
-                "process.env": {
-                    NODE_ENV: JSON.stringify(isDevBuild ? "development" : "production")
-                }
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require("./wwwroot/dist/vendor-manifest.json")
-            })
-        ].concat(isDevBuild ? [
-            // Plugins that apply in development builds only
-            new webpack.SourceMapDevToolPlugin({
-                filename: "[file].map", // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(bundleOutputDir, "[resourcePath]") // Point sourcemap entries to the original file locations on disk
-            })
-        ] : [
-                // Plugins that apply in production builds only
-                new webpack.optimize.UglifyJsPlugin(),
-                new ExtractTextPlugin("site.css")
-            ])
-    }];
+module.exports = {
+    entry: {
+        "main": "./ClientApp/boot.ts"
+    },
+    output: {
+        path: path.join(__dirname, "./wwwroot/dist"),
+        filename: "[name].js",
+        publicPath: "dist/"
+    },
+    resolve: {
+        // Makes sure the paths set in tsconfig will load (baseurl)
+        extensions: [".js", ".ts"],
+        plugins: [ new TsConfigPathsPlugin() ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue\.html$/,
+                include: /ClientApp/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.ts$/,
+                include: /ClientApp/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                test: /\.less$/,
+                include: /ClientApp/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            }
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
 };

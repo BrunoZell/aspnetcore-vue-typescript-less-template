b396ce4abeaa14c368ec26d034023f90c951ba40
Bruno Zell <bruno.zzell@gmail.com>
2018-07-31 19:27:57 +0200

    better Vuetiful app layout

--- src/Template.Web/ClientApp/components/app/app.ts
+++ src/Template.Web/ClientApp/components/app/app.ts
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import * as V from "vuetify/es5/components/*";
 
 @Component
 export default class AppComponent extends Vue {
+    menuOpen: boolean = false;
+    menuItems = [
+        { icon: "settings", text: "Settings" },
+        { icon: "chat_bubble", text: "Send feedback" },
+        { icon: "help", text: "Help" }
+    ];
 }

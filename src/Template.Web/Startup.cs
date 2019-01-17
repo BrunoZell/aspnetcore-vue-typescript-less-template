e9195e97863e41ce79b227d78bae9f4a37f05122
Bruno Zell <bruno.zzell@gmail.com>
2019-01-17 06:14:23 +0100

    Simplify Startup.cs

--- src/Template.Web/Startup.cs
+++ src/Template.Web/Startup.cs
@@ -4,20 +4,15 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OdeToCode.AddFeatureFolders;
-using System;
 
 namespace Template.Web
 {
     public class Startup
     {
         public IConfiguration Configuration { get; }
-        public IHostingEnvironment Environment { get; }
 
-        public Startup(IConfiguration configuration, IHostingEnvironment environment)
-        {
-            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-            Environment = environment ?? throw new ArgumentNullException(nameof(environment));
-        }
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
 
         public void ConfigureServices(IServiceCollection services)
         {

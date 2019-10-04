7dd5083dd5f78d198b9d9489e49d005ef9e98434
Bruno Zell <bruno.zzell@gmail.com>
2019-10-04 08:45:34 +0200

    add default analyzers

--- src/VueTemplate.Web/Startup.cs
+++ src/VueTemplate.Web/Startup.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OdeToCode.AddFeatureFolders;
+using System.Diagnostics.CodeAnalysis;
 
 namespace VueTemplate.Web
 {
+    [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "ASP.Net Core expects instance methods in Startup classes")]
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
